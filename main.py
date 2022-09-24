3b77dc49c284d4ea97413831b9e1406b487385b8
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2022-09-24 22:02:45 +0200

    basic GUI

---
+++ main.py
@@ -0,0 +1,71 @@
+from PyQt5 import QtWidgets, uic, QtCore
+from typing import List
+import sys
+
+
+class DataWidget(QtWidgets.QWidget):
+    doubleClicked = QtCore.pyqtSignal()
+
+    def __init__(self, column: int, teams: QtCore.QStringListModel) -> None:
+        super().__init__()
+        uic.loadUi("data.ui", self)
+        self.column = column
+        self.name.setModel(teams)
+
+    def setTime(self, time: float):
+        self.doubleSpinBox_time.setValue(time)
+
+    def reset(self):
+        self.name.setCurrentText("")
+        self.time.setValue(0)
+        self.invalid.setChecked(False)
+
+    def mouseDoubleClickEvent(self, event):
+        self.doubleClicked.emit()
+
+
+class RunRow(QtCore.QObject):
+    doubleClicked = QtCore.pyqtSignal()
+
+    def __init__(
+        self,
+        num: int,
+        teams: QtCore.QStringListModel,
+        grid: QtWidgets.QGridLayout,
+        row: int,
+        col: int = 1,
+    ) -> None:
+        super().__init__()
+        self.num = num
+        self.widgets = [DataWidget(i, teams) for i in range(3)]
+        for c, w in enumerate(self.widgets):
+            grid.addWidget(w, row, col + c)
+            w.doubleClicked.connect(self.doubleClicked)
+
+
+class Race:
+    def __init__(self, grid: QtWidgets.QGridLayout) -> None:
+        self.teams = QtCore.QStringListModel()
+        self.runs = [RunRow(i, self.teams, grid, 5 + i) for i in range(3)]
+        for w in self.runs:
+            w.doubleClicked.connect(lambda: print("doubleClicked"))
+
+    def setTeamNames(self, names: List[str]):
+        names.append("")
+        self.teams.setStringList(names)
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+        uic.loadUi("main.ui", self)  # load .ui file
+
+        self.race = Race(self.gridLayout)
+        self.race.setTeamNames(["1", "2", "3"])
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    app.exec_()
