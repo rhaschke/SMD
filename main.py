4535f0fb676042c2550b89a84a4c96a22b904e40
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2022-10-02 10:58:53 +0200

    Gracefully quit on Ctrl-C

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from PyQt5 import QtWidgets, QtCore
 from gui import MainWindow
 import sys
+import signal
 
+
+def sigint_handler(*args):
+    print(" Quitting")
+    QtWidgets.QApplication.quit()
 
 app = QtWidgets.QApplication(sys.argv)
 
 main = MainWindow()
 main.nextRace()
+
+# Run the python interpreter every 100ms to catch Ctrl-C
+timer = QtCore.QTimer()
+timer.start(100)
+timer.timeout.connect(lambda: None)
+# Call our custom signal handler on Ctrl-C
+signal.signal(signal.SIGINT, sigint_handler)
 
 main.show()
 app.exec_()
